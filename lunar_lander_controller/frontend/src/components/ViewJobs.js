70a70aa5c7050e4d5353d0b4876e38d20c3c1632
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-01-26 23:49:57 +0000

    Display created jobs in table

--- lunar_lander_controller/frontend/src/components/ViewJobs.js
+++ lunar_lander_controller/frontend/src/components/ViewJobs.js
@@ -1,13 +1,59 @@
 import React, { Component } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 export default class ViewJobs extends Component {
-  constructor(props) {
-    super(props);
+  state = { jobs: [] };
+
+  componentDidMount() {
+    fetch("/api/job")
+      .then((response) => response.json())
+      .then((data) => this.setState({ jobs: data }));
   }
 
   render() {
     return (
-        <p>View Jobs</p>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Code</TableCell>
+              <TableCell>Complete</TableCell>
+              <TableCell>Crash Reward</TableCell>
+              <TableCell>Land Reward</TableCell>
+              <TableCell>Left Leg Reward</TableCell>
+              <TableCell>Right Leg Eeward</TableCell>
+              <TableCell>Main Engine Reward</TableCell>
+              <TableCell>Side Engine Reward</TableCell>
+              <TableCell>Created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.jobs.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="left">{row.code}</TableCell>
+                <TableCell align="left">{row.complete === false? "False": "True" }</TableCell>
+                <TableCell align="left">{row.crash_reward}</TableCell>
+                <TableCell align="left">{row.land_reward}</TableCell>
+                <TableCell align="left">{row.left_leg_reward}</TableCell>
+                <TableCell align="left">{row.right_leg_reward}</TableCell>
+                <TableCell align="left">{row.main_engine_reward}</TableCell>
+                <TableCell align="left">{row.side_engine_reward}</TableCell>
+                <TableCell align="left">{row.created_at}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
   }
 }
