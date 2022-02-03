b126bd125a47332cd4f345e763eaa30dba0a9f74
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-02-03 17:53:08 +0000

    Run rl from view when job is created

--- lunar_lander_controller/api/views.py
+++ lunar_lander_controller/api/views.py
@@ -4,6 +4,7 @@ from .serializers import JobSerializer, CreateJobSerializer
 from .models import Job
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from subprocess import Popen
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ from rest_framework.response import Response
 class JobView(generics.ListAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
 
 class GetJob(APIView):
     serializer_class = JobSerializer
@@ -27,6 +29,7 @@ class GetJob(APIView):
 
         return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class CreateJobView(APIView):
     serializer_class = CreateJobSerializer
 
@@ -40,8 +43,12 @@ class CreateJobView(APIView):
             main_engine_reward = serializer.data.get('main_engine_reward')
             side_engine_reward = serializer.data.get('side_engine_reward')
             job = Job(crash_reward=crash_reward, land_reward=land_reward, first_leg_reward=first_leg_reward,
-                            second_leg_reward=second_leg_reward,main_engine_reward=main_engine_reward,side_engine_reward=side_engine_reward)
+                      second_leg_reward=second_leg_reward, main_engine_reward=main_engine_reward, side_engine_reward=side_engine_reward)
             job.save()
+
+            p = Popen(['python', 'reinforcement_learning/rl.py', str(crash_reward), str(land_reward),
+                       str(first_leg_reward), str(second_leg_reward), str(main_engine_reward), str(side_engine_reward)])
+
             return Response(JobSerializer(job).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
