f9abc46a9cddd090f1de2935a08fc6e66dd64fe9
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-02-18 12:01:31 +0000

    Add multiple algorithms to serializer

--- lunar_lander_controller/api/serializers.py
+++ lunar_lander_controller/api/serializers.py
@@ -7,11 +7,11 @@ class JobSerializer(serializers.ModelSerializer):
         model = Job
         fields = ('id', 'code', 'complete', 'crash_reward',
                   'land_reward','first_leg_reward','second_leg_reward',
-                  'main_engine_reward','side_engine_reward', 'created_at',"algorithm")
+                  'main_engine_reward','side_engine_reward', 'created_at','dqn','a2c','ddpg','her','ppo','sac')
 
 class CreateJobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ('crash_reward','land_reward',
                   'first_leg_reward','second_leg_reward',
-                  'main_engine_reward','side_engine_reward',"algorithm")
+                  'main_engine_reward','side_engine_reward','dqn','a2c','ddpg','her','ppo','sac')
